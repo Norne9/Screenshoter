d157b34f698eb1c76d82664d89dd1c6c1275fb35
Aleksey Pestov <a.pestov@datasakura.com>
2021-05-28 20:43:28 +0500

    UI done

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Screenshoter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Screenshoter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationForTextBox(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out var _);
+        }
+    }
+}
